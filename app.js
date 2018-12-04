02d0e76ebe715dc18bc8de4b6a77dd1816122f71
Ryan <Rhackemer@gmail.com>
2018-12-04 15:41:10 -0700

    update click listener

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const formication = document.querySelector("form")
 const button = document.querySelector("button")
+const synsOfTheZaddy = document.querySelector("synsOfTheZaddy")
+const input = document.querySelector("input")
 
-formication.addEventListener("submit", submission)
+button.addEventListener("click", submission)
 
-function submission(event)
+function submission(click) {
+    input.innerHTML = ""
+    event.preventDefault()
+    const syn = input.value
+    getSyns(syn)
+    console.log()
+}
 
-fetch("http://words.bighugelabs.com/api/2/a872a264aa49d0016e40aec5ac21215e/")
-    .then(function (response) {
-        return response.json()
-    })
-    .then(function (data) {
-        console.log(data)
-    })
-    .catch(function (err) {
-        console.log(err.message)
-    })
+function getSyns(syn) {
+    const BASE_URL = "http://words.bighugelabs.com/api/2/a872a264aa49d0016e40aec5ac21215e/" + syn + "/json"
+
+    fetch(BASE_URL)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            let synData = data
+            synsOfTheZaddy.innerHTML = synData.verb.syn[0]
+            console.log(synData.verb.syn[0])
+
+        })
+        .catch(function (err) {
+            console.log(err.message)
+        })
+}
